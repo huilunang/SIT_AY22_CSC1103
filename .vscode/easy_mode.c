005a0a8cfec703369eecff74028a968e1d0ddcc8
mit-day <timothy.tzx@gmail.com>
2022-11-20 02:57:08 +0800

    still wip

--- .vscode/easy_mode.c
+++ .vscode/easy_mode.c
@@ -1,121 +1,130 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<time.h>
+#include "main.h"
+#include "players.h"
+#include "utils.h"
 
-char gridChar(int i) {
-    switch(i) {
-        case -1:
-            return 'X';
-        case 0:
-            return ' ';
-        case 1:
-            return 'O';
+char *PLAYER1 = "X";
+char *PLAYER2 = "O";
+
+// Player
+void playerMove(GtkWidget *grid, gpointer data) {
+    // Get grid position
+    GtkLayoutManager *layoutManager = gtk_widget_get_layout_manager(grid);
+    GtkLayoutChild *layoutChild = gtk_layout_manager_get_layout_child(layoutManager, data);
+    int row = gtk_grid_layout_child_get_row(GTK_GRID_LAYOUT_CHILD(layoutChild));
+    int col = gtk_grid_layout_child_get_column(GTK_GRID_LAYOUT_CHILD(layoutChild));
+
+    gtk_button_set_label(data, curPlayer);
+    board[row][col] = *curPlayer;
+    --blankSpaces;
+}
+
+// AI
+/* NOTE: DO THE EASY MINMAX CHECK HERE */
+// Easy MINIMAX still wip
+/*int minimax(char board[3][3], int player) {
+    checkWinner(winner);
+    int addWinner = (*winner == 'O') ? 1 : -1;
+    if (*winner != ' ') {
+        *winner = ' ';
+        return addWinner * player;
     }
-}
-
-void draw(int b[9]) {
-    printf(" %c | %c | %c\n",gridChar(b[0]),gridChar(b[1]),gridChar(b[2]));
-    printf("---+---+---\n");
-    printf(" %c | %c | %c\n",gridChar(b[3]),gridChar(b[4]),gridChar(b[5]));
-    printf("---+---+---\n");
-    printf(" %c | %c | %c\n",gridChar(b[6]),gridChar(b[7]),gridChar(b[8]));
-}
-
-int win(const int board[9]) {
-    //determines if a player has won, returns 0 otherwise.
-    unsigned wins[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    int i;
-    for(i = 0; i < 8; ++i) {
-        if(board[wins[i][0]] != 0 &&
-           board[wins[i][0]] == board[wins[i][1]] &&
-           board[wins[i][0]] == board[wins[i][2]])
-            return board[wins[i][2]];
-    }
-    return 0;
-}
-
-int minimax(int board[9], int player) {
-    //How is the position like for player (their turn) on board?
-    int winner = win(board);
-    int move,counter = 0;
-    if(winner != 0) return winner*player;
-    move = -1;
-    int score = -2;//Losing moves are preferred to no move
-    int i;
-    srand(time(0));
-    for(i = 0; i < 9; ++i) {//For all moves,
-        if(board[i] == 0) {//If legal,
-            if ( rand() % 10 >= 4) { // if rand() greater than 4 breaks the loop meaning 60% chance for ai to be stupid
-                  continue; 
-            }
-            board[i] = player;//Try the move
-            int thisScore = -minimax(board, player*-1);
-            if(thisScore > score) {
-                score = thisScore;
-                move = i;
-            }//Pick the one that's worst for the opponent
-            board[i] = 0;//Reset board after try
-        }
-    }
-    if(move == -1) return 0;
-    return score;
-}
-
-void computerMove(int board[9]) {
     int move = -1;
-    int score = -2;
-    int i;
-    for(i = 0; i < 9; ++i) {
-        if(board[i] == 0) {
-            board[i] = 1;
-            int tempScore = -minimax(board, -1);
-            board[i] = 0;
-            if(tempScore > score) {
-                score = tempScore;
-                move = i;
+    // Losing moves are preferred to no move
+    int bestScore = -2;
+    srand(0)
+    for (int row = 0; row < 3; ++row) {
+        for (int col = 0; col < 3; ++col) {
+            // Board position is empty
+            if (board[row][col] == ' ') {
+                if ( rand() % 10 >= 4) { 
+                  board[i] = rand() % 9;
+                }
+                // Try the move
+                char playSymbol = (player == 1) ? 'O' : 'X';
+                board[row][col] = playSymbol;
+
+                int score = -minimax(board, player*-1);
+
+                // Pick the move that's worst for the opponent
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = 1;
+                }
+
+                // Reset board position after try
+                board[row][col] = ' ';
             }
         }
     }
-    //returns a score based on minimax tree at a given node.
-    board[move] = 1;
-}
+    // AI never make any movement
+    if (move == -1)
+        return 0;
+    return bestScore;
+}*/
 
-void playerMove(int board[9]) {
-    int move = 0;
-    do {
-        printf("\nInput move ([0..8]): ");
-        scanf("%d", &move);
-        printf("\n");
-    } while (move >= 9 || move < 0 || board[move] != 0);
-    board[move] = -1;
-}
 
-int main() {
-    int board[9] = {0,0,0,0,0,0,0,0,0};
-    //computer squares are 1, player squares are -1.
-    printf("Computer: O, You: X\nPlay (1)st or (2)nd? ");
-    int player=0;
-    scanf("%d",&player);
-    printf("\n");
-    unsigned turn;
-    for(turn = 0; turn < 9 && win(board) == 0; ++turn) {
-        if((turn+player) % 2 == 0)
-            computerMove(board);
-        else {
-            draw(board);
-            playerMove(board);
+
+
+
+void computerMove(char board[3][3], GtkGrid *grid) {
+    int bestScore = -2;
+    for (int row = 0; row < 3; ++row) {
+        for (int col = 0; col < 3; ++col) {
+            if (board[row][col] == ' ') {
+                board[row][col] = 'O';
+                // AI: 1, Human Player: -1
+                int score = -minimax(board, -1);
+                board[row][col] = ' ';
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    aiMove[0] = row;
+                    aiMove[1] = col;
+                }
+            }
         }
     }
-    switch(win(board)) {
-        case 0:
-            printf("A draw. How droll.\n");
-            break;
-        case 1:
-            draw(board);
-            printf("You lose.\n");
-            break;
-        case -1:
-            printf("You win. Inconceivable!\n");
-            break;
+
+    GtkWidget *button = gtk_grid_get_child_at(grid, aiMove[1], aiMove[0]);
+    gtk_button_set_label(GTK_BUTTON(button), curPlayer);
+    board[aiMove[0]][aiMove[1]] = *curPlayer;
+    --blankSpaces;
+}
+
+int minimax(char board[3][3], int player) {
+    checkWinner(winner);
+    int addWinner = (*winner == 'O') ? 1 : -1;
+    if (*winner != ' ') {
+        *winner = ' ';
+        return addWinner * player;
     }
+    int move = -1;
+    // Losing moves are preferred to no move
+    int bestScore = -2;
+    for (int row = 0; row < 3; ++row) {
+        for (int col = 0; col < 3; ++col) {
+            // Board position is empty
+            if (board[row][col] == ' ') {
+        
+                // Try the move
+                char playSymbol = (player == 1) ? 'O' : 'X';
+                board[row][col] = playSymbol;
+
+                int score = -minimax(board, player*-1);
+
+                // Pick the move that's worst for the opponent
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = 1;
+                }
+
+                // Reset board position after try
+                board[row][col] = ' ';
+            }
+        }
+    }
+    // AI never make any movement
+    if (move == -1)
+        return 0;
+    return bestScore;
 }
