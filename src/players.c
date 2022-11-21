3aa01170b740084bbb153bddf40c8e052894f5cb
huilunang <57522846+huilunang@users.noreply.github.com>
2022-11-21 21:52:46 +0800

    Add easy minimax (#5)
    * Add easy minimax

--- src/players.c
+++ src/players.c
@@ -7,26 +7,56 @@ char *PLAYER2 = "O";
 
 // Player
 void playerMove(GtkWidget *grid, gpointer data) {
-    // Get grid position
+    // Get grid position from GUI
     GtkLayoutManager *layoutManager = gtk_widget_get_layout_manager(grid);
     GtkLayoutChild *layoutChild = gtk_layout_manager_get_layout_child(layoutManager, data);
     int row = gtk_grid_layout_child_get_row(GTK_GRID_LAYOUT_CHILD(layoutChild));
     int col = gtk_grid_layout_child_get_column(GTK_GRID_LAYOUT_CHILD(layoutChild));
 
+    // update GUI
     gtk_button_set_label(data, curPlayer);
+
+    // set to occupy board
     board[row][col] = *curPlayer;
     --blankSpaces;
 }
 
 // AI
-/* NOTE: DO THE EASY MINMAX CHECK HERE */
 void computerMove(char board[3][3], GtkGrid *grid) {
+
+    if (aiEasyMode == TRUE) {
+        srand(time(0));
+        
+        // enable 60% of chance to have AI randomly choose a position
+        if (rand() % 10 >= 4) {
+            // get all the empty position coordinates in the board
+            checkEmptyPos(boardEmptyPos);
+            // randomly select a position to place the move
+            int randPos = rand() % countEmptyPos;
+
+            // update GUI
+            GtkWidget *button = gtk_grid_get_child_at(grid,
+                                                    boardEmptyPos[randPos][1],
+                                                    boardEmptyPos[randPos][0]);
+            gtk_button_set_label(GTK_BUTTON(button), curPlayer);
+
+            // set to occupy board
+            board[boardEmptyPos[randPos][0]][boardEmptyPos[randPos][1]] = *PLAYER2;
+            --blankSpaces;
+
+            // reset the board empty position for next use
+            memset(boardEmptyPos, 0, sizeof(boardEmptyPos[0][0]) * 9 * 2);
+
+            return;
+        }
+    }
+
     int bestScore = -2;
 
     for (int row = 0; row < 3; ++row) {
         for (int col = 0; col < 3; ++col) {
             if (board[row][col] == ' ') {
-                board[row][col] = 'O';
+                board[row][col] = *PLAYER2;
                 // AI: 1, Human Player: -1
                 int score = -minimax(board, -1);
                 board[row][col] = ' ';
@@ -40,15 +70,18 @@ void computerMove(char board[3][3], GtkGrid *grid) {
         }
     }
 
+    // update GUI
     GtkWidget *button = gtk_grid_get_child_at(grid, aiMove[1], aiMove[0]);
     gtk_button_set_label(GTK_BUTTON(button), curPlayer);
+
+    // set to occupy board
     board[aiMove[0]][aiMove[1]] = *curPlayer;
     --blankSpaces;
 }
 
 int minimax(char board[3][3], int player) {
     checkWinner(winner);
-    int addWinner = (*winner == 'O') ? 1 : -1;
+    int addWinner = (*winner == *PLAYER2) ? 1 : -1;
     if (*winner != ' ') {
         *winner = ' ';
         return addWinner * player;
@@ -63,7 +96,7 @@ int minimax(char board[3][3], int player) {
             // Board position is empty
             if (board[row][col] == ' ') {
                 // Try the move
-                char playSymbol = (player == 1) ? 'O' : 'X';
+                char playSymbol = (player == 1) ? *PLAYER2 : *PLAYER1;
                 board[row][col] = playSymbol;
 
                 int score = -minimax(board, player*-1);
--- src/utils.h
+++ src/utils.h
@@ -1,14 +1,17 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
-char board[3][3];
-char curPlayer[2];
-char winner[2];
 int blankSpaces;
+char board[3][3];
+int boardEmptyPos[9][2];
+int countEmptyPos;
+char curPlayer[2];
 int reset;
-int modeP1;
+char winner[2];
 int aiEasyMode;
+int modeP1;
 
+void checkEmptyPos(int boardEmptyPos[9][2]);
 void checkWinner(char *winner);
 int checkWinnerOrDraw();
 void incCounterScore(GtkLabel *obj);
